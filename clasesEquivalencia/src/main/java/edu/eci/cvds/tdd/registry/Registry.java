d0cf359b86b931d8f3aebe40877f220ff12c30cb
Derjasai <elholaquehace88@gmail.com>
2022-02-09 22:16:30 -0500

    Se finaliza el laboratorio

--- clasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ clasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -2,17 +2,24 @@ package edu.eci.cvds.tdd.registry;
 
 public class Registry {
     public RegisterResult registerVoter(Person p) {
-		RegisterResult result;
+		
+		RegisterResult result = null;
+		
         // TODO Validate person and return real result.
 		if(p.isAlive() && (p.getAge()>=18)){
-			result = RegisterResult.VALID
+			
+			result = RegisterResult.VALID;
 		}else if(!p.isAlive()){
+			
 			result = RegisterResult.DEAD;
-		}else if(p.getAge()<18){
+		}else if(p.getAge()<18 && p.getAge() >= 0){
+			
 			result = RegisterResult.UNDERAGE;
-		}else if(p.getAge()){
+		}else if(p.getAge() < 0){
+			
 			result = RegisterResult.INVALID_AGE;
 		}
+		
 		return result;
     }
 }
